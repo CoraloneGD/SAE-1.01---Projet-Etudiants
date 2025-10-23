5752fc0f78d4f203c7b79d0a1d3f5275c38fc915
Coralone <168082997+CoraloneGD@users.noreply.github.com>
2025-10-23 15:26:51 +0200

    Update main.c 3

--- main.c
+++ main.c
@@ -23,24 +23,19 @@ typedef struct {
 	int semestres;  // max 6 semestres
 	int id;
 	int UE;
-	float note;
+	float note[NB_SEMESTRES][NB_UE];
 } Etudiant;
 
 typedef struct {
 	Etudiant liste_etudiants[NB_ETUDIANTS_MAX];
-	int id_etudiant;
+	int nombre_etudiant;
 } IdEtudiant;
 
 typedef struct {
-	IdEtudiant liste_UE[NB_UE];
-	float note;
-} NotesUE;
+	int UE[NB_UE];
+} MesUE;
 
 
-
-Etudiant nouv_etudiant;
-IdEtudiant id;
-NotesUE notesUE;
 
 
 int EXIT(char commande[NB_CARACTERES]) {
@@ -51,7 +46,7 @@ int EXIT(char commande[NB_CARACTERES]) {
 	return 0;
 }
 
-int INSCRIRE(void) {
+int INSCRIRE(IdEtudiant id, Etudiant nouv_etudiant) {
 	char nom[NB_CARACTERES];
 	char prenom[NB_CARACTERES];
 
@@ -64,13 +59,13 @@ int INSCRIRE(void) {
 	}
 
 	// Vérifier la limite max d'étudiants
-	if (id.id_etudiant >= NB_ETUDIANTS_MAX) {
+	if (id.nombre_etudiant >= NB_ETUDIANTS_MAX) {
 		printf("Limite d'etudiants atteinte (%d). Inscription refusee.\n", NB_ETUDIANTS_MAX);
 		return -1;
 	}
 
 	// vérifie si l'étudiant ne possède pas le même prenom et nom que l'ancien (majuscule ou min
-	for (int j = 0; j < id.id_etudiant; ++j) {
+	for (int j = 0; j < id.nombre_etudiant; ++j) {
 		if (_stricmp(id.liste_etudiants[j].nom, nom) == 0 && (_stricmp(id.liste_etudiants[j].prenom, prenom) == 0)) {
 			printf("Etudiant deja enregistre \n");
 			return -1;
@@ -91,12 +86,12 @@ int INSCRIRE(void) {
 	nouv_etudiant.semestres = 0;
 
 	// Générer un ID unique
-	int nouvel_id = id.id_etudiant;
+	int nouvel_id = id.nombre_etudiant;
 	int id_existe = 1;
 
 	while (id_existe) {
 		id_existe = 0;
-		for (int i = 0; i < id.id_etudiant; ++i) {
+		for (int i = 0; i < id.nombre_etudiant; ++i) {
 			if (id.liste_etudiants[i].id == nouvel_id) {
 				// si l'ID existe déjà, on passe au suivant
 				id_existe = 1;
@@ -108,20 +103,20 @@ int INSCRIRE(void) {
 
 	// Assigner l'ID et ajouter à la liste
 	nouv_etudiant.id = nouvel_id;
-	id.liste_etudiants[id.id_etudiant] = nouv_etudiant;
-	id.id_etudiant++;
+	id.liste_etudiants[id.nombre_etudiant] = nouv_etudiant;
+	id.nombre_etudiant++;
 
 	printf("Inscription enregistree (%d)\n", nouvel_id);
 	return 0;
 }
 
-int ETUDIANTS(void) {
-	if (id.id_etudiant == 0) {
+int ETUDIANTS(IdEtudiant id, Etudiant nouv_etudiant) {
+	if (id.nombre_etudiant == 0) {
 		printf("Aucun etudiant n'est enregistre.\n");
 		return 0;
 	}
 	else {
-		for (int i = 0; i < id.id_etudiant; ++i) {
+		for (int i = 0; i < id.nombre_etudiant; ++i) {
 			printf("%d - %s %s - S%d - %s\n", id.liste_etudiants[i].id, id.liste_etudiants[i].nom, id.liste_etudiants[i].prenom,
 				id.liste_etudiants[i].semestres, id.liste_etudiants[i].statut);
 		}
@@ -129,11 +124,11 @@ int ETUDIANTS(void) {
 	return 0;
 }
 
-int NOTE(void) {
-	int id_etudiant = 0;
+int NOTE(IdEtudiant id, Etudiant nouv_etudiant) {
+	int id_etudiant;
 	// Vérifie si l'ID est valide.
-	scanf("%d %d %f", &id_etudiant, &nouv_etudiant.UE, &notesUE.liste_UE->liste_etudiants[id_etudiant].note);
-	for (int i = 0; i < id.id_etudiant; ++i) {
+	scanf_s("%d %d %f", &id_etudiant, &nouv_etudiant.UE, &id.liste_etudiants->note);
+	for (int i = 0; i < id.nombre_etudiant; ++i) {
 		if (id_etudiant == id.liste_etudiants[i].id) {
 			printf("Note enregistree \n");
 		}
@@ -145,33 +140,33 @@ int NOTE(void) {
 }
 
 
-int CURSUS(void) {
-	int id_etudiant;
-	char status[3];
-	scanf_s("%d", &id_etudiant);
-	// Boucle qui vérifie si l'id étudiant correspond à celui du tableau (1 == 1).
-	for (int i = 0; i < id.id_etudiant; ++i) {
-		if (id_etudiant == id.liste_etudiants[i].id) {
-			printf("%d %s %s\n", id.liste_etudiants[i].id, id.liste_etudiants[i].nom, id.liste_etudiants[i].prenom);
-		}
-
-		for (int j = 1; j < id.liste_etudiants[i].semestres; ++j) {
-			for (int k = 1; k < id.liste_etudiants[i].UE; ++k) {
-				for (int l = 1; l < notesUE.liste_UE->liste_etudiants[i].note; ++l) {
-					if (notesUE.liste_UE->liste_etudiants[i].note >= ADMIS) {
-						status[0] = "ADM";
-					}
-					else {
-						status[0] = "AJ";
-					}
-
-					printf("- %dd (%s)\n", notesUE.liste_UE->liste_etudiants[l].note, status);
-				}
-			}
-		}
-	}
-	return 0;
-}
+//int CURSUS(void) {
+//	int id_etudiant;
+//	char status[3];
+//	scanf_s("%d", &id_etudiant);
+//	// Boucle qui vérifie si l'id étudiant correspond à celui du tableau (1 == 1).
+//	for (int i = 0; i < id.id_etudiant; ++i) {
+//		if (id_etudiant == id.liste_etudiants[i].id) {
+//			printf("%d %s %s\n", id.liste_etudiants[i].id, id.liste_etudiants[i].nom, id.liste_etudiants[i].prenom);
+//		}
+//
+//		for (int j = 1; j < id.liste_etudiants[i].semestres; ++j) {
+//			for (int k = 1; k < id.liste_etudiants[i].UE; ++k) {
+//				for (int l = 1; l < notesUE.liste_UE->liste_etudiants[i].note; ++l) {
+//					if (notesUE.liste_UE->liste_etudiants[i].note >= ADMIS) {
+//						status[0] = "ADM";
+//					}
+//					else {
+//						status[0] = "AJ";
+//					}
+//
+//					printf("- %dd (%s)\n", notesUE.liste_UE->liste_etudiants[l].note, status);
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
 
 
 
@@ -179,6 +174,10 @@ int CURSUS(void) {
 int main() {
 	char commande[NB_CARACTERES];
 	int recommencer = 1;
+	Etudiant nouv_etudiant = { 0 }
+;
+	IdEtudiant id;
+	
 
 	do {
 		printf("Entrez une commande parmis celles qui sont disponibles, en MAJUSCULE obligatoire ! \n");
@@ -186,13 +185,13 @@ int main() {
 		scanf("%32s", commande);
 
 		if (strcmp(commande, "ETUDIANTS") == 0) {
-			ETUDIANTS();
+			ETUDIANTS(id, nouv_etudiant);
 		}
 		else if (strcmp(commande, "CURSUS") == 0) {
-			CURSUS();
+			//CURSUS(id, nouv_etudiant);
 		}
 		else if (strcmp(commande, "INSCRIRE") == 0) {
-			INSCRIRE();
+			INSCRIRE(id, nouv_etudiant);
 		}
 		else if (strcmp(commande, "EXIT") == 0) {
 			EXIT(commande);
@@ -200,11 +199,14 @@ int main() {
 		else if (strcmp(commande, "JURY") == 0 ) {
 			printf("Organisation du jury... \n");
 		}
-		else if (strcmp(commande, "BILAN") == 0) {
-			printf("Affichage du bilan des étudiants... \n");
-		}
+
 		else if (strcmp(commande, "NOTE") == 0) {
-			NOTE();
+			NOTE(id, nouv_etudiant);
+
+		}
+
+		else if (strcmp(commande, "DEMISSION") == 0) {
+			printf("Affichage du bilan des étudiants... \n");
 		}
 		else {
 			printf("Commande inconnue. Veuillez reessayer.\n");
